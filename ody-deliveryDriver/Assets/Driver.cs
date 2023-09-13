f10145deab245a682821b7757e4e68df1e3e0b48
Odysseus-Tales <142748027+Odysseus-Tales@users.noreply.github.com>
2023-09-13 11:12:14 -0400

    Using Time.deltaTime

--- ody-deliveryDriver/Assets/Driver.cs
+++ ody-deliveryDriver/Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.5f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 275f;
+    [SerializeField] float moveSpeed = 7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +15,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerAmount = UnityEngine.Input.GetAxis("Horizontal") * steerSpeed;
-        float moveAmount = UnityEngine.Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = UnityEngine.Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = UnityEngine.Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
     }
