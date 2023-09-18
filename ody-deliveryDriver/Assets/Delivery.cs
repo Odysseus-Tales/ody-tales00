c5c03873afe8d7f6fa9f5df8247137453fc3628d
Odysseus-Tales <142748027+Odysseus-Tales@users.noreply.github.com>
2023-09-18 17:27:16 -0400

    how to use bools

--- ody-deliveryDriver/Assets/Delivery.cs
+++ ody-deliveryDriver/Assets/Delivery.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("I'm Walking over here");
@@ -12,9 +13,11 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag =="Package"){
             Debug.Log("package picked up!");
+            hasPackage = true;
         }
-        if (other.tag =="Customer"){
+        if (other.tag =="Customer"&& hasPackage){
             Debug.Log("package Delivered!");
+            hasPackage = false;
         }
     }
  }
