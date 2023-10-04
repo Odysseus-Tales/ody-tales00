6be545ea76fd9f79cfb3a7369d68340135aaba6d
Odysseus-Tales <142748027+Odysseus-Tales@users.noreply.github.com>
2023-10-04 11:48:53 -0400

    prevent double play

--- snowBoard/Assets/Scripts/CrashDetector.cs
+++ snowBoard/Assets/Scripts/CrashDetector.cs
@@ -8,9 +8,11 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
 
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag== "Ground"){
+        if(other.tag== "Ground"&& hasCrashed == false){
+            hasCrashed = true;
             FindObjectOfType<PlayerControl>().Disablecontrols();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
